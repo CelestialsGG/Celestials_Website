130b3913b6e1415908ab4d7e5e9e8d7b7379cfe2
parker gagliano <pjg4086@uncw.edu>
2022-10-24 13:26:39 -0400

    streams and offline formatted correctly, now just need personal styling

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 let clinetId = "1jld8u8wgq6gptu0aqtn4pwi3icyo4";
 let clinetSecret = "xt2ee0rwaj9vuyvbpf6qjiwz8apo2o";
-let channelList=['bigrodentt', 'tragiicisbad', 'loltyler1', 'tarik', 'shroud', 'emongg', 'landonkyle', 'summit1g']
+let channelList=['m0xyy','bigrodentt', 'tragiicisbad', 'loltyler1', 'tarik', 'shroud', 'emongg', 'landonkyle', 'summit1g']
 
 function getTwitchAuthorization() {
     let url = `https://id.twitch.tv/oauth2/token?client_id=${clinetId}&client_secret=${clinetSecret}&grant_type=client_credentials`;
@@ -56,71 +56,124 @@ function renderStreams(data) {
     let streamsContainer = document.getElementById("joemama");
     let offlineContainer = document.getElementById("offlineContainer");
     streamsContainer.innerHTML = "";
-    let temprow = document.createElement('div')
-    temprow.classList='row justify-content-between'
+    let temprow = []
+    let temprowonline = 0
 
-    console.log(channelList)
-    let ttee = 0
+
+
+    let counter = 0
     streams.forEach((stream) => {
-        ttee +=1
-        console.log('test')
-        let { thumbnail_url: thumbnail, title, viewer_count, user_name} = stream;
+        let { thumbnail_url: thumbnail, title, viewer_count, user_name, game_name} = stream;
         let hdThumbnail = thumbnail
             .replace("{width}", "1280")
             .replace("{height}", "720");
 
-        temprow.innerHTML += `
 
-            <div class="col-md-4 col-sm-12 mt-2 border">
+        if (counter % 3 == 0 && counter !=0) {
+            temprowoffiline = createElement('row mt-4 justify-content-around', temprow)
+            temprow = []
+            streamsContainer.appendChild(temprowoffiline)
+            
+        }
+        counter +=1
+
+        temprow.push(`
+            <a style="text-decoration: none!important;"href="https://twitch.tv/${user_name}" class="col-md-4 col-sm-12 mt-2 border p-0">
                 <div class="row">
                 <img class=" img-fluid"src="${hdThumbnail}" alt="" />
                 </div>
                 <div class="row">
-                <div class="col"><h5 style="font-weight: bold">${title}</h5></div>
+                <div class="col"><h5 class="text-center text-light" style="font-weight: bold">${title}</h5></div>
                 </div>
+
                 <div class="row">
-                    <div class="col-auto">
+                
+                    <div class="col">
                     <h5 class="text-left">${user_name}</h5>
                     </div>
-                    <div class="col"><h5>${viewer_count
+                    <div class="col-4"><h5 class="text-center text-light">${viewer_count
                         .toString()
-                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")} Viewers</h5></div>
-                
-            </div>
-            
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")} Viewers</h5 class="text-center text-light"></div>
+                    <div class="col"><h5 class="text-center text-light">${game_name}</h5></div>
+                </div>
+               
+            </a>
+        `)
 
-        `;
-        console.log(temprow)
-        if (ttee === 3) {
-            streamsContainer.appendChild(temprow)
-
-        }
-        if (channelList.includes(stream.user_name)) {
-            channelList.splice(channelList.indexOf(stream.user_name), 1);
-        }
     });
-    streamsContainer.appendChild(temprow)
-    
-    
-    let temp = 0
-    let tempBool = true
-    let row = document.createElement('div')
-    row.className='row mt-3'
-    for (let i=0; i<channelList.length; i++) {
-        let oneElement = document.createElement('div')
-        oneElement.className = "col-md-4 col-sm-12 mt-2"
+    if (temprow.length ===1) {
 
-        temp+=1
-        offlineContainer.innerHTML += `
-        <div class="col-auto">
-        <a style="text-decoration: none!important;"href=https://twitch.tv/${channelList[i]}>
+        temprow.push(`
+            <div class="col-12 col-md-3">
+            </div> 
+            `)
+            temprow.push(`
+        <div class="col-12 col-md-3">
+        </div> 
+        `)
+    }
+    if (temprow.length === 2) {
+        temprow.push(`
+            <div class="col-12 col-md-3">
+            </div> 
+            `)
+    }
+    temprowonline = createElement('row mt-4 justify-content-around', temprow)
+    streamsContainer.appendChild(temprowonline)
+
+    
+    let temprowoffiline = 2
+    let row = []
+    for (let i=0; i<channelList.length; i++) {
+        if (i % 3 == 0 && i !=0) {
+
+            temprowoffiline = createElement('row mt-4 justify-content-around', row)
+            row = []
+
+            offlineContainer.appendChild(temprowoffiline)
+            
+        }
+        row.push(`
+        <div class="col-12 col-md-3">
+        <a style="text-decoration: none!important;"href="https://twitch.tv/${channelList[i]}">
         <h5 class="text-center text-light">${channelList[i]}</h5></a>
         </div> 
-        `
+        `)
     }
+    if (row.length ===1) {
+
+        row.push(`
+            <div class="col-12 col-md-3">
+            </div> 
+            `)
+        row.push(`
+        <div class="col-12 col-md-3">
+        </div> 
+        `)
+    }
+    if (row.length === 2) {
+        row.push(`
+            <div class="col-12 col-md-3">
+            </div> 
+            `)
+    }
+    temprowoffiline = createElement('row mt-4 justify-content-around', row)
+    offlineContainer.appendChild(temprowoffiline)
+
 
     
     
+
+}
+
+
+function createElement(classes, content) {
+    let final = document.createElement('div')
+    final.className = classes
+    for (let i = 0; i < content.length; i++) {
+        final.innerHTML+= content[i]
+    }
+    return final
 
 }
 
